b6489da614a3c3600e05199dad3a7d47dac3585a
jayphodges <jayphodges@gmail.com>
2017-11-05 13:19:38 -0700

    Added format validations for mac address on the model level

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,6 +1,8 @@
 class Device < ApplicationRecord
   validates :name, presence: true
-  validates :mac, presence: true, uniqueness: true
-  
+  validates :mac, presence: true, uniqueness: true, length: { maximum: 17 },
+            format: { with: /[0-9a-f]{2}([-:])[0-9a-f]{2}(\\1[0-9a-f]{2}){4}$/,
+            message: "Invalid MAC format"}
+
   belongs_to :user
 end
